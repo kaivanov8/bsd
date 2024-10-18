7ab5f4078d14f5f02ece456dce162235526bd2e9
kaivanov8 <kosiv6799@gmail.com>
2024-10-18 20:22:10 +0700

    Add files via upload

---
+++ bsd.py
@@ -0,0 +1,13 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99 > 9.98 and 1000!=1000.1)
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+# 4th program
+str_1= '123.456'
+q = float (str_1)
+q=int(q*10)%10
+print (q)
